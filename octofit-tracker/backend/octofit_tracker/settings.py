1cee4d0925f0e5e908663ce58c68fa0a8b8d4989
khrystyna-drozdova <khrystyna_drozdova@epam.com>
2026-03-22 20:22:49 +0000

    4

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,12 +26,12 @@ SECRET_KEY = 'django-insecure-l%$iv_vgex#5s)od-pkdt97nm4&3bo69op#$mfl(@6x4i_-$9m
 DEBUG = True
 
 
-# Allow all hosts for development
-ALLOWED_HOSTS = ['*']
-
+# Allow all hosts for development and codespace
 import os
-if os.environ.get('CODESPACE_NAME'):
-    ALLOWED_HOSTS.append(f"{os.environ.get('CODESPACE_NAME')}-8000.app.github.dev")
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    ALLOWED_HOSTS.append(f"{codespace_name}-8000.app.github.dev")
 
 
 # Application definition
